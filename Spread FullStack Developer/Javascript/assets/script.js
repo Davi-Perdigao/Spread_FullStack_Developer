139bd9a218cc63f140de432cc0af63a5b91140dc
Davi Perdigão <davivcperdigao@gmail.com>
2022-05-02 16:33:29 -0300

    Contador

---
+++ Spread FullStack Developer/Javascript/assets/script.js
@@ -0,0 +1,33 @@
+var currentNumberWrapper = document.getElementById('currentNumber')
+var count = 0
+var increment = document.getElementById('increment')
+var decrement = document.getElementById('decrement')
+
+// condição para contador funcionar 
+// 0 <= count =< 10
+
+increment.addEventListener("click",
+    function increment(){
+        count++;
+        if(count <= 10){
+            currentNumberWrapper.innerHTML = count;
+        }
+        if(count >= 0){
+            currentNumberWrapper.style = "color: black;"
+        }
+
+    }
+)
+
+decrement.addEventListener("click",
+    function decrement(){
+        count--;
+        if(count >= -10 ){
+            currentNumberWrapper.innerHTML = count;
+        }
+
+        if(count < 0){
+            currentNumberWrapper.style = "color: red;"
+        }
+    }
+);
