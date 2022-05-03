47c6e61e21565f786dce59dfbca528d0f163ca05
Davi Perdigão <davivcperdigao@gmail.com>
2022-05-03 10:17:00 -0300

    Contador OK

--- Spread FullStack Developer/Javascript/Contador/assets/script.js
+++ Spread FullStack Developer/Javascript/Contador/assets/script.js
@@ -1,14 +1,21 @@
 var currentNumberWrapper = document.getElementById('currentNumber')
-var count = 0
+count = 0;
 var increment = document.getElementById('increment')
 var decrement = document.getElementById('decrement')
 
+/*Tente implementar os eventos usando o método addEventListener.
+Crie condicionais que desabilitam o botão de incrementar ou decrementar quando count chegar a um determinado valor.
+Mude a cor do texto em CURRENT_NUMBER para vermelho quando o número for negativo.
+*/
 
 increment.addEventListener("click",
     function increment(){
-        count++;
-        if(count >= 0){
-            currentNumberWrapper.style = "color: black;"
+        if (Number(currentNumber.textContent) < 10) {
+            count ++;
+            currentNumber.textContent = Number(currentNumber.textContent) + 1;
+            if (count >= 0){
+                currentNumberWrapper.style = "color: black;"
+            }
         }
 
     }
@@ -16,9 +23,12 @@ increment.addEventListener("click",
 
 decrement.addEventListener("click",
     function decrement(){
-        count--;
-        if(count < 0){
-            currentNumberWrapper.style = "color: red;"
+        if (Number(currentNumber.textContent) > -10) {
+            count --;
+            currentNumber.textContent = Number(currentNumber.textContent) - 1;
+            if (count < 0){
+                currentNumberWrapper.style = "color: red;"
+            }
         }
     }
 );
