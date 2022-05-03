15b1a6a3bca563d80e9a40126b8ecea26263e863
Davi Perdigão <davivcperdigao@gmail.com>
2022-05-03 11:08:11 -0300

    To Do

---
+++ Spread FullStack Developer/Javascript/To Do List/assets/script.js
@@ -0,0 +1,128 @@
+var taskInput = document.getElementById("new-task");
+var addButton = document.getElementsByTagName("button")[0];
+var incompleteTaskHolder = document.getElementById("incomplete-tasks");
+var completedTasksHolder = document.getElementById("completed-tasks");
+
+var createNewTaskElement = function (taskString) {
+  var listItem = document.createElement("li");
+
+  //input (checkbox)
+  var checkBox = document.createElement("input");
+  //label
+  var label = document.createElement("label");
+  //input (text)
+  var editInput = document.createElement("input");
+  //button.edit
+  var editButton = document.createElement("button");
+  //button.delete
+  var deleteButton = document.createElement("button");
+
+  label.innerText = taskString;
+
+  checkBox.type = "checkbox";
+  editInput.type = "text";
+  editButton.innerText = "Edit";
+  editButton.className = "edit";
+  deleteButton.innerText = "Delete";
+  deleteButton.className = "delete";
+
+  listItem.appendChild(checkBox);
+  listItem.appendChild(label);
+  listItem.appendChild(editInput);
+  listItem.appendChild(editButton);
+  listItem.appendChild(deleteButton);
+  return listItem;
+};
+
+//Adicionando nova tarefa
+var addTask = function () {
+  console.log("Adicionando tarefa...");
+  var listItem = createNewTaskElement(taskInput.value);
+
+  incompleteTaskHolder.appendChild(listItem);
+  bindTaskEvents(listItem, taskCompleted);
+
+  taskInput.value = "";
+};
+
+//Editando uma tarefa existente
+var editTask = function () {
+  console.log("Editando tarefa...");
+  console.log("Change 'edit' to 'save'");
+
+  var listItem = this.parentNode;
+
+  var editInput = listItem.querySelector("input[type=text]");
+  var label = listItem.querySelector("label");
+  var containsClass = listItem.classList.contains("editMode");
+  if (containsClass) {
+    label.innerText = editInput.value;
+  } else {
+    editInput.value = label.innerText;
+  }
+  listItem.classList.toggle("editMode");
+};
+
+//Detando tarefa
+var deleteTask = function () {
+  console.log("Deletando tarefa...");
+
+  var listItem = this.parentNode;
+  var ul = listItem.parentNode;
+  ul.removeChild(listItem);
+};
+
+//Marcando tarefa como completa
+var taskCompleted = function () {
+  console.log("Tarefa Concluída...");
+
+  //Adicionando a tarefa à lista de concluído
+  var listItem = this.parentNode;
+  completedTasksHolder.appendChild(listItem);
+  bindTaskEvents(listItem, taskIncomplete);
+};
+
+var taskIncomplete = function () {
+  console.log("Tarefa incompleta...");
+  //Marcando tarefa como incompleta quando a checkboc está desmarcada
+  var listItem = this.parentNode;
+  incompleteTaskHolder.appendChild(listItem);
+  bindTaskEvents(listItem, taskCompleted);
+};
+
+var ajaxRequest = function () {
+  console.log("AJAX Request");
+};
+
+//Setando click para função de adicionar tarefa
+addButton.onclick = addTask;
+addButton.addEventListener("click", addTask);
+addButton.addEventListener("click", ajaxRequest);
+
+var bindTaskEvents = function (taskListItem, checkBoxEventHandler) {
+  console.log("bind list item events");
+  //selecionando ListItems children
+  var checkBox = taskListItem.querySelector("input[type=checkbox]");
+  var editButton = taskListItem.querySelector("button.edit");
+  var deleteButton = taskListItem.querySelector("button.delete");
+
+  //Vincular editTask ao botão de edição
+  editButton.onclick = editTask;
+  //Vincular deleteTask ao botão delete
+  deleteButton.onclick = deleteTask;
+  //Tarefa de vinculação concluída 
+  checkBox.onchange = checkBoxEventHandler;
+};
+
+//percorre os itens da lista ul incompleta
+//para cada item da lista
+for (var i = 0; i < incompleteTaskHolder.children.length; i++) {
+  //vincular eventos aos itens da lista children(tasksCompleted)
+  bindTaskEvents(incompleteTaskHolder.children[i], taskCompleted);
+}
+
+//percorre os itens da lista de ul de concluídas
+for (var i = 0; i < completedTasksHolder.children.length; i++) {
+  //vincular eventos aos itens da lista children(tasksIncompleted)
+  bindTaskEvents(completedTasksHolder.children[i], taskIncomplete);
+}
