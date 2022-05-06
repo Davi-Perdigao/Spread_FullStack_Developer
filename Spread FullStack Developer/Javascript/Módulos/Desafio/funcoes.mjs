65e5d6e3d2452975855fd78480b81cf267cf2657
Davi Perdigão <davivcperdigao@gmail.com>
2022-05-06 11:03:30 -0300

    desafio modulos OK

---
+++ Spread FullStack Developer/Javascript/Módulos/Desafio/funcoes.mjs
@@ -0,0 +1,11 @@
+export function mostraIdade(nome, idade) {
+	return `A idade de ${nome} é ${idade} anos.`;
+}
+
+export function mostraCidade(nome, cidade) {
+	return `A cidade de ${nome} é ${cidade}.`;
+}
+
+export function mostraHobby(nome, hobby) {
+	return `O hobby de ${nome} é ${hobby}!`;
+}
---
+++ Spread FullStack Developer/Javascript/Módulos/Desafio/main.mjs
@@ -0,0 +1,7 @@
+import { mostraIdade, mostraCidade, mostraHobby } from './funcoes.mjs';
+
+console.log(mostraIdade('Davi', 20));
+
+console.log(mostraCidade('Davi', 'Divinópolis-MG'));
+
+console.log(mostraHobby('Davi', 'Futebol'));
