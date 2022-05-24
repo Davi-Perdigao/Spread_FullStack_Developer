bef69d2062b4352c4cf4f0eaee14303db04e8fc9
Davi Perdigão <davivcperdigao@gmail.com>
2022-05-24 15:24:02 -0300

    js and ts

--- Spread FullStack Developer/Javascript/Estacionamento/script.ts
+++ Spread FullStack Developer/Javascript/Estacionamento/script.ts
@@ -1,3 +1,12 @@
 (function(){
-    
-})
+    const $ = (query: string): HTMLInputElement | null => document.querySelector(query);
+    $("#cadastrar")?.addEventListener("click", () => {
+        const nome = $("#nome")?.value;
+        const placa = $("#placa")?.value;
+
+        if(nome || placa) {
+            alert("Os campos nome e placa são obrigatórios!");
+            return;
+        }
+    })
+})();
