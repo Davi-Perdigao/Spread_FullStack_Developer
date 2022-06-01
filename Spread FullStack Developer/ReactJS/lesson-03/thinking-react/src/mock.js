06afc7f0e7c41153f4bdde20c4d098689ee0ebe8
Davi Perdigão <davivcperdigao@gmail.com>
2022-06-01 12:08:21 -0300

    thinking react

---
+++ Spread FullStack Developer/ReactJS/lesson-03/thinking-react/src/mock.js
@@ -0,0 +1,24 @@
+const mock = [
+    {
+      id: 1,
+      name: 'Davi Perdigão',
+      skills: ['React', 'Node', 'CSS', 'Webpack']
+    },
+    {
+      id: 2,
+      name: 'Beltrano Ali',
+      skills: ['HTML', 'React Native', 'Go', 'JS']
+    },
+    {
+      id: 3,
+      name: 'Fulano de Tal',
+      skills: ['Assembly']
+    },
+    {
+      id: 4,
+      name: 'José Ciclano',
+      skills: ['Reason']
+    }
+  ]
+  
+  export default mock
