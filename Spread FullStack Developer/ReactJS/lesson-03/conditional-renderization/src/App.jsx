e6ef47996693f6f6f0792ff09829d6834f8b757f
Davi Perdigão <davivcperdigao@gmail.com>
2022-06-01 11:57:06 -0300

    conditional renderization

---
+++ Spread FullStack Developer/ReactJS/lesson-03/conditional-renderization/src/App.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+const buttonA = <button>Histórico dos Clientes</button>
+
+const buttonB = <button>Cadastrar Cliente</button>
+
+const hasCustomer = true
+
+const App = () => {
+
+  const renderShowHistory = (
+    <div>
+      Clique no botão abaixo para visualizar o histórico dos clientes
+      <br />
+      {buttonA}
+    </div>
+  )
+
+  const renderAddCustomer = (
+    <div>
+      Clique abaixo para cadastrar o cliente
+      <br />
+      {buttonB}
+    </div>
+  )
+
+  const showCustomer = () => {
+
+    if (!hasCustomer) return null
+
+    return (
+      <div>
+        <h1>Nome do Cliente: Davi Perdigão</h1>
+      </div>
+    )
+  }
+
+  console.log('hasCustomer', hasCustomer)
+
+
+  return (
+    <div>
+      <p>Digital Innovation One</p>
+      <p>Bem vindo a nossa aula =D.</p>
+      {hasCustomer ? renderShowHistory : renderAddCustomer}
+      <div>
+        {showCustomer()}
+      </div>
+    </div>
+  );
+};
+export default App;
