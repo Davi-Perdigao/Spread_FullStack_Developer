e999bbd734a9d659148cee199dda32bb577950fa
Davi Perdigão <davivcperdigao@gmail.com>
2022-06-01 11:48:31 -0300

    webpack config

---
+++ Spread FullStack Developer/ReactJS/lesson-02/webpack-config/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from './Button'
+const App = () => {
+  return (
+    <div>
+      <p>Digital Innovation One</p>
+      <p>Bem vindo a nossa aula =D.</p>
+      <div>Webpack 4</div>
+      <Button />
+    </div>
+  );
+};
+export default App;
---
+++ Spread FullStack Developer/ReactJS/lesson-02/webpack-config/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  devtool: 'source-map',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundler.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+}
